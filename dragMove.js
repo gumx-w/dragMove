dd41d31e8359e6a00b948b45434bb8114066a5f7
mx-mac <mail@gumx.top>
2019-02-16 22:08:58 +0800

    修改引用方式

--- dragMove.js
+++ dragMove.js
@@ -1,63 +1,73 @@
-/**
- * 拖拽移动函数，只需要初始化一次。不许时调用destory函数进行注销
- * @param {[type]} dragEl      要拖动的元素
- * @param {[type]} targetEl 拖动事件生效的元素
- */
-function DragMove(dragEl,targetEl,option) {
-    var stx, sty, ox, oy;
+(function (root, factory) {
+    if(typeof exports === 'object' && typeof module === 'object')
+        module.exports = factory();
+    else if(typeof define === 'function' && define.amd)
+        define([], factory);
+    else if(typeof exports === 'object')
+        exports["DragMove"] = factory();
+    else
+        root["DragMove"] = factory();
+})(this,function(){
+    /**
+     * 拖拽移动函数，只需要初始化一次。不许时调用destory函数进行注销
+     * @param {[type]} dragEl      要拖动的元素
+     * @param {[type]} targetEl 拖动事件生效的元素
+     */
+    function DragMove(dragEl,targetEl,option) {
+        var stx, sty, ox, oy;
+
+        option = option || {};
+        var boxShadow = option.boxShadow ||  'darkgrey 10px 10px 30px 5px ';
+
+        targetEl.style.cursor = 'move';
+        targetEl.style.userSelect = 'none';
+
+        dragEl.style.left = dragEl.offsetLeft + 'px';
+        dragEl.style.top = dragEl.offsetTop + 'px';
+        dragEl.style.width = dragEl.offsetWidth + 'px';   
+        dragEl.style.height = dragEl.offsetHeight + 'px';
+        dragEl.style.boxShadow = boxShadow;
 
 
-    option = option || {};
-    var boxShadow = option.boxShadow ||  'darkgrey 10px 10px 30px 5px ';
+        var startHandler = function (event) {
 
-    targetEl.style.cursor = 'move';
-    targetEl.style.userSelect = 'none';
-
-    dragEl.style.left = dragEl.offsetLeft + 'px';
-    dragEl.style.top = dragEl.offsetTop + 'px';
-    dragEl.style.width = dragEl.offsetWidth + 'px';   
-    dragEl.style.height = dragEl.offsetHeight + 'px';
-    dragEl.style.boxShadow = boxShadow;
+            stx = event.pageX;
+            sty = event.pageY;
+            ox = dragEl.offsetLeft;
+            oy = dragEl.offsetTop;
 
 
-    var startHandler = function (event) {
+            dragEl.style.position = 'absolute';
+            dragEl.style.left = ox + 'px';
+            dragEl.style.top = oy + 'px';
 
-        stx = event.pageX;
-        sty = event.pageY;
-        ox = dragEl.offsetLeft;
-        oy = dragEl.offsetTop;
+            document.addEventListener('mousemove', moveHandler);
+            document.addEventListener('mouseup', endHandler);
+        }
 
+        var moveHandler = function (event) {
+            console.log(1)
+            let mx = event.pageX - stx;
+            let my = event.pageY - sty;
 
-        dragEl.style.position = 'absolute';
-        dragEl.style.left = ox + 'px';
-        dragEl.style.top = oy + 'px';
+            dragEl.style.left = ox + mx + 'px';
+            dragEl.style.top = oy + my + 'px';
+        }
 
-        document.addEventListener('mousemove', moveHandler);
-        document.addEventListener('mouseup', endHandler);
+        var endHandler = function (event) {
+            document.removeEventListener('mousemove', moveHandler);
+            document.removeEventListener('mouseup', endHandler);
+        }
+
+        this.destory = function () {
+            targetEl.removeEventListener('mousedown', startHandler);
+        }
+
+        targetEl.addEventListener('mousedown', startHandler);
     }
-
-    var moveHandler = function (event) {
-        console.log(1)
-        let mx = event.pageX - stx;
-        let my = event.pageY - sty;
-
-        console.log(mx)
-console.log(my)
+    return DragMove;
+})
 
 
-        dragEl.style.left = ox + mx + 'px';
-        dragEl.style.top = oy + my + 'px';
-    }
 
-    var endHandler = function (event) {
-        document.removeEventListener('mousemove', moveHandler);
-        document.removeEventListener('mouseup', endHandler);
-    }
-
-    this.destory = function () {
-        targetEl.removeEventListener('mousedown', startHandler);
-    }
-
-    targetEl.addEventListener('mousedown', startHandler);
-}
 
